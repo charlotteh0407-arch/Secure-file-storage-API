bbda36b3398f0560383bb22c4ac75e6a6df6495e
Charlotte Holford <18holfordc@roundwoodpark.co.uk>
2026-08-24 15:19:43 +0100

    feat implement validation of authorization header

---
+++ app/api/auth.py
@@ -0,0 +1,35 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import session
+from app.database import SessionLocal
+from app.models.user import User
+from app.Logic.security import decode_access_token
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def get_current_user(token: str=Depends(oauth2_scheme), db: Session = Depends(get_db)) ->User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials"
+    )
+
+    payload = decode_access_token(token)
+    if payload is None:
+        raise credentials_exception
+
+    user_id = payload.get("sub")
+    if user_id is None:
+        raise credentials_exception
+
+    user = db.query(User).filter(User.id == int(user_id)).first()
+    if user is None:
+        raise credentials_exception
+
+    return user
